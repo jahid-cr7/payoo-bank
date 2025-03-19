fcb08294cc30841c10c102af4aab6da090c4e8e0
jahid-cr7 <jahidhasan@stu.scu.edu.cn>
2025-03-19 17:16:29 +0800

    Pay bill Done

--- toogle/toogle.js
+++ toogle/toogle.js
@@ -1,5 +1,6 @@
 document.getElementById("cashout-sec").style.display = "none";
 document.getElementById("transer-money-sec").style.display = "none";
+document.getElementById("paybill-sec").style.display = "none";
 document.getElementById("add-money-box").addEventListener("click", function () {
   document.getElementById("add-money-sec").style.display = "block";
 });
@@ -8,9 +9,17 @@ document.getElementById("cash-out-box").addEventListener("click", function () {
   document.getElementById("add-money-sec").style.display = "none";
   document.getElementById("cashout-sec").style.display = "block";
   document.getElementById("transer-money-sec").style.display = "none";
+  document.getElementById("paybill-sec").style.display = "none";
 });
 document.getElementById("transfer").addEventListener("click", function () {
   document.getElementById("add-money-sec").style.display = "none";
   document.getElementById("cashout-sec").style.display = "none";
   document.getElementById("transer-money-sec").style.display = "block";
+  document.getElementById("paybill-sec").style.display = "none";
+});
+document.getElementById("paybill").addEventListener("click", function () {
+  document.getElementById("add-money-sec").style.display = "none";
+  document.getElementById("cashout-sec").style.display = "none";
+  document.getElementById("transer-money-sec").style.display = "none";
+  document.getElementById("paybill-sec").style.display = "block";
 });
